6ca3c672f3b074c4712bc7abcd5fe71ba7ff8cf1
SRIJAN SARASWAT <srijansaraswat2003@gmail.com>
2025-09-22 00:38:32 +0530

    Updated something

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
+    # processing - database, cache, rendering HTML template
     return HttpResponse("Hello, world. You're at the polls index.")
